b6cb1cf523db052e1a971709e57d3521884e2645
MinhDuc <duclmhe173155@fpt.edu.vn>
2025-06-06 14:09:16 +0700

    update change role

--- server/routers/SaleManager/saleManager.js
+++ server/routers/SaleManager/saleManager.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Authozation = require("../../middleware/auth")
 const controller = require("../../controller/SaleManager/SaleManager");
+const { authorizeRoles } = require("../../middleware/auth");
 
 router.get("/getAllSaleManager", controller.getAllSaleManager);
-router.put("/changeRole/:id", Authozation.authenticateToken, controller.changeRole);
+router.put("/changeRole/:id", Authozation.authenticateToken, authorizeRoles(2), controller.changeRole);
 
 module.exports = router;
 
